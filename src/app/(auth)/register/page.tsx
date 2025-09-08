1a03893399cdedb1962b0fd218e4fff56ecc1de2
hoangtuanqn <phamhoangtuanqn@gmail.com>
2025-09-08 16:39:13 +0700

    Setup project structure part 2

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h2>Đây là trang đăng ký</h2>
+        </div>
+    );
+};
+
+export default RegisterPage;
+
