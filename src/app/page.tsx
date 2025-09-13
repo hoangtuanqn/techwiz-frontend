5532b4ec4834cdb6b383b9628957731e31e18031
hoangtuanqn <phamhoangtuanqn@gmail.com>
2025-09-13 19:50:57 +0700

    Fix hero part 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import React from "react";
 
 import Hero from "./_components/Hero";
 import Features from "./_components/Features";
-<<<<<<< HEAD
 // import Categories from "./_components/Categories";
 import Stats from "./_components/Stats";
-=======
->>>>>>> f9ce292e0507ccee7e7819f4f1e9ce3a09c5b236
 import Testimonials from "./_components/Testimonials";
 import Blogs from "./_components/Blogs";
 import Cta from "./_components/Cta";
